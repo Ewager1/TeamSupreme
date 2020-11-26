fff8fc4fe4d9ea29248c3392efb03ab4d5817412
EWager <ethanbwager@gmail.com>
2020-11-26 18:23:56 -0500

    created engineer subclass and succesfully passed paremeters from Employee

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+//import Employee Class
+import Employee from './Employee.js';
+
+//create Engineer subclass 
+class Engineer extends Employee {
+    constructor(name,id, email, github) {
+        super(name,id,email)
+        this.github = github
+    }
+    getGithub() {return github}
+    getRole() {return "Engineer"}
+}
+
+let joe = new Engineer('Joe', 32, 'joe@joe.com', 'git@hub.com')
+
+console.log(joe)
