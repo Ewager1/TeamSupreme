fc2b0ff93261a14a413be9605513ba4fa9418490
EWager <ethanbwager@gmail.com>
2020-12-01 18:29:19 -0500

    added question logic that adds questions based on job position chosen

--- Develop/app.js
+++ Develop/app.js
@@ -1,86 +1,61 @@
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
-const inquirer = require("inquirer.js");
+const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-//inquierer:
-
-//manager inquierer questions
-let getManagerInfo = [
-  { type: "input", name: "managerName", message: "Enter Manger's Full Name" },
-  { type: "input", name: "managerId", message: "Enter Manager's ID" },
+var questions = [
   {
-    type: "input",
-    name: "mangerEmail",
-    message: "Enter Manager's contact Email",
+    type: "list",
+    name: "position",
+    message: "Which position would you like to add?",
+    choices: ["Manager", "Engineer", "Intern"],
   },
   {
     type: "input",
-    name: "manager_name",
-    message: "Enter Manager's Office Number",
+    name: "fullName",
+    message: "What is your full name?",
   },
-];
-
-//intern inquirer questions
-let getInternInfo = [
   {
     type: "input",
-    name: "internName",
-    message: "Enter Intern's Full Name",
+    name: "email",
+    message: "What is your email?",
   },
-
-  { type: "input", name: "internId", message: "Enter Intern's ID" },
-
   {
     type: "input",
-    name: "internEmail",
-    message: "Enter Intern's contact Email",
+    name: "Office Number",
+    message: "Manager, what is your office number?",
+    when: (answers) => answers.position === "Manager",
   },
-
   {
     type: "input",
-    name: "internSchool",
-    message: "Enter Intern's Office Number",
+    name: "companyID",
+    message: "What is your company ID?",
   },
-];
-
-//engineer inquierer questions
-let getEngineerInfo = [
   {
     type: "input",
-    name: "engineerName",
-    message: "Enter Engineer's Full Name",
+    name: "GitHub URL",
+    message: "Engineer, what is your GitHub URL?",
+    when: (answers) => answers.position === "Engineer",
   },
-
-  { type: "input", name: "engineerID", message: "Enter Engineer's ID" },
-
   {
     type: "input",
-    name: "engineerEmail",
-    message: "Enter Engineer's contact Email",
-  },
-
-  {
-    type: "input",
-    name: "engineerGithub",
-    message: "Enter Engineer's Office Number",
+    name: "SchoolName",
+    message: "Intern, what is your School Name?",
+    when: (answers) => answers.position === "Intern",
   },
 ];
 
-inquirer.prompt(getManagerInfo).then((managerInfo) => {
-  console.log(JSON.stringify(managerInfo));
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers));
 });
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
