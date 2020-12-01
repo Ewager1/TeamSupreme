759fd3d0358f5bade16e2ba2f0f70c93c94f013d
EWager <ethanbwager@gmail.com>
2020-12-01 11:40:01 -0500

    added inquirer questions for manager, engineer, intern in app.js

--- Develop/app.js
+++ Develop/app.js
@@ -10,9 +10,67 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+//inquierer:
+
+//manager inquierer questions
+let getManagerInfo = {
+  type: "input",
+  name: "managerName",
+  message: "Enter Manger's Full Name",
+
+  type: "input",
+  name: "managerId",
+  message: "Enter Manager's ID",
+
+  type: "input",
+  name: "mangerEmail",
+  message: "Enter Manager's contact Email",
+
+  type: "input",
+  name: "manager_name",
+  message: "Enter Manager's Office Number",
+};
+
+//intern inquirer questions
+let getInternInfo = {
+  type: "input",
+  name: "internName",
+  message: "Enter Intern's Full Name",
+
+  type: "input",
+  name: "internId",
+  message: "Enter Intern's ID",
+
+  type: "input",
+  name: "internEmail",
+  message: "Enter Intern's contact Email",
+
+  type: "input",
+  name: "internSchool",
+  message: "Enter Intern's Office Number",
+};
+
+//engineer inquierer questions
+let getEngineerInfo = {
+  type: "input",
+  name: "engineerName",
+  message: "Enter Engineer's Full Name",
+
+  type: "input",
+  name: "engineerID",
+  message: "Enter Engineer's ID",
+
+  type: "input",
+  name: "engineerEmail",
+  message: "Enter Engineer's contact Email",
+
+  type: "input",
+  name: "engineerGithub",
+  message: "Enter Engineer's Office Number",
+};
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
