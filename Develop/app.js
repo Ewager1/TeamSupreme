5cee94906f1abe8c07b6f48981b8dd694f6049c0
EWager <ethanbwager@gmail.com>
2020-12-02 09:24:30 -0500

    created fs.write function that writes employees into team.html

--- Develop/app.js
+++ Develop/app.js
@@ -62,6 +62,7 @@ var questions = [
 //placeholder array for employee objects
 employeeArray = [];
 
+//runs the questions prompt, calculates which position each answer is, adds to employee array, then renders
 function runQuestions() {
   inquirer.prompt(questions).then((answers) => {
     switch (answers.position) {
@@ -93,32 +94,21 @@ function runQuestions() {
           answers.schoolName
         );
         employeeArray.push(intern);
-        break;
     }
 
     if (answers.addPosition === "yes") {
       runQuestions();
-    }
+    } else makeTemplate();
   });
 }
 
+//starts program by running questions
 runQuestions();
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//Basic FS.write copied and edited from W3 schools
+function makeTemplate() {
+  fs.writeFile(outputPath, render(employeeArray), function (err) {
+    if (err) throw err;
+    console.log("Error");
+  });
+}
